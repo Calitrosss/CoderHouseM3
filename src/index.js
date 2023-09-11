6973e394588ebe9fa0c3796aa6495103acb8e776
Carlos Vivas <carsed@hotmail.com>
2023-09-11 01:39:06 -0300

    fix index title, description and bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Style
